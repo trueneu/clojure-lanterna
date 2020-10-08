e65dbe231eae8f898c9f65cb53226f082c349e22
Michiel Borkent <michielborkent@gmail.com>
2020-10-08 22:49:16 +0200

    Deployment env vars

--- project.clj
+++ project.clj
@@ -6,6 +6,6 @@
   :java-source-paths ["./java"]
   :profiles {:dev {:dependencies [[org.clojure/clojure "1.10.2-alpha1"]]}}
   :repositories {"releases" {:url "https://clojars.org/repo"
-                             :username :env/babashka_nrepl_clojars_user
-                             :password :env/babashka_nrepl_clojars_pass
+                             :username :env/clojars_user
+                             :password :env/clojars_pass
                              :sign-releases false}})

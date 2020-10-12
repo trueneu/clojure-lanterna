29cfef253edb3d795478a5d21619201fcfca94b9
Michiel Borkent <michielborkent@gmail.com>
2020-10-12 17:08:32 +0200

    deploy

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
   :dependencies [[com.googlecode.lanterna/lanterna "3.0.3"]]
   :java-source-paths ["./java"]
   :profiles {:dev {:dependencies [[org.clojure/clojure "1.10.2-alpha1"]]}}
-  :repositories {"releases" {:url "https://clojars.org/repo"
-                             :username :env/clojars_user
-                             :password :env/clojars_pass
-                             :sign-releases false}})
+  :deploy-repositories [["clojars" {:url "https://clojars.org/repo"
+                                    :username :env/clojars_user
+                                    :password :env/clojars_pass
+                                    :sign-releases false}]])
